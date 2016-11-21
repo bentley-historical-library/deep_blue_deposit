f9b48b66e0a56b1167a4d19ab71ce14abcbcafb0
Max <eckardm@umich.edu>
2016-11-21 13:52:27 -0500

    another days work

--- archivematica_aip_to_dspace_api/archivematica_aip_to_dspace_api.py
+++ archivematica_aip_to_dspace_api/archivematica_aip_to_dspace_api.py
@@ -2,6 +2,11 @@ import os
 import subprocess
 from lxml import etree
 import shutil
+import requests
+from lxml.builder import E
+from slacker import Slacker
+
+from credentials import *
 
 # `\media\sf_DeepBlue` is auto-mounted
 deep_blue_saf_staging = os.path.join(
@@ -34,7 +39,7 @@ for root, _, files in os.walk(deep_blue_saf_staging):
 
 for aip_dir in aip_dirs:
 
-    print "processing " + aip_dir
+    print "\nprocessing " + aip_dir
 
     print "  * getting metadata"
 
@@ -51,6 +56,10 @@ for aip_dir in aip_dirs:
     first_name = user.split(", ")[1].split('"')[1].split('"')[0]
     last_name = user.split(", ")[2].split('"')[1].split('"')[0]
 
+    print "\nUsername | First Name | Last Name"
+    print "--- | --- | ---"
+    print " | ".join([username, first_name, last_name])
+
     accession_no = [event.xpath("./premis:eventOutcomeInformation/premis:eventOutcomeDetail/premis:eventOutcomeDetailNote", namespaces=namespaces)[0].text.split("#")[1] for event in tree.xpath("//premis:event", namespaces=namespaces) if event.xpath("./premis:eventType", namespaces=namespaces)[0].text == "registration"][0]
 
     dcterms_title = tree.xpath(".//dc:title", namespaces=namespaces)[0].text
@@ -60,12 +69,11 @@ for aip_dir in aip_dirs:
     dcterms_rights_copyright = tree.xpath(".//dc:rights", namespaces=namespaces)[0].text
     dcterms_relation_ispartofseries = ""
 
-    print "  * getting permissions"
+    print "\ndc.title | dc.description.abstract | dc.contributor.author | dc.date.issued | dc.rights.copyright | dc.relation.ispartofseries"
+    print "--- | --- | --- | --- | --- | ---"
+    print " | ".join([dcterms_title, dcterms_description_abstract, dcterms_contributor_author, dcterms_date_issued, dcterms_rights_copyright, dcterms_relation_ispartofseries])
 
-    reading_room = False
-    university_of_michigan = False
-    bentley_digital_media_library = False
-    um_records = False  # also need date and DISALLOW, althought the latter not 'till later
+    print "\n  * getting rights statements"
 
     act = tree.xpath(".//premis:act", namespaces=namespaces)[0].text
     restriction = tree.xpath(".//premis:restriction", namespaces=namespaces)[0].text
@@ -73,38 +81,122 @@ for aip_dir in aip_dirs:
     end_date = tree.xpath(".//premis:endDate", namespaces=namespaces)[0].text
     rights_granted_note = tree.xpath(".//premis:rightsGrantedNote", namespaces=namespaces)[0].text
 
-    print "  * splitting package into objects and metadata zips"
+    print "\nAct | Restriction | Start | End | Grant/restriction Note"
+    print "--- | --- | --- | --- | ---"
+    print " | ".join([act, restriction, start_date, end_date, rights_granted_note])
 
-    objects_dir = os.path.join(aip_dir, "data", "objects", [name for name in os.listdir(os.path.join(aip_dir, "data", "objects")) if name.startswith("digital_object_component")][0])
+    print "\n  * splitting package into objects and metadata zips"
 
-    objects_zip = os.path.join(aip_dir, "objects.7z")
-
-    command = [
-        "7z", "a",  # add
-        "-bd",  # disable percentage indicator
-        "-t7z",  # type of archive
-        "-y",  # assume yes on all queries
-        "-m0=bzip2",  # compression method
-        "-mtc=on", "-mtm=on", "-mta=on",  # keep timestamps (create, mod, access)
-        "mmt=on",  # multithreaded
-        objects_zip,  # destination
-        objects_dir  # source
-    ]
-    subprocess.call(command)
+    # objects_dir = os.path.join(aip_dir, "data", "objects", [name for name in os.listdir(os.path.join(aip_dir, "data", "objects")) if name.startswith("digital_object_component")][0])
+    # objects_zip = os.path.join(aip_dir + "-temp", "objects.7z")
+    # command = [
+        # "7z", "a",  # add
+        # "-bd",  # disable percentage indicator
+        # "-t7z",  # type of archive
+        # "-y",  # assume yes on all queries
+        # "-m0=bzip2",  # compression method
+        # "-mtc=on", "-mtm=on", "-mta=on",  # keep timestamps (create, mod, access)
+        # "-mmt=on",  # multithreaded
+        # objects_zip,  # destination
+        # objects_dir  # source
+    # ]
+    # subprocess.call(command)
 
     # shutil.rmtree(objects_dir)
 
-    metadata_zip = os.path.join(aip_dir, "metadata.7z")
-    command = [
-        "7z", "a",  # add
-        "-bd",  # disable percentage indicator
-        "-t7z",  # type of archive
-        "-y",  # assume yes on all queries
-        "-m0=bzip2",  # compression method
-        "-mtc=on", "-mtm=on", "-mta=on",  # keep timestamps (create, mod, access)
-        "mmt=on",  # multithreaded
-        "-x!objects.7z",  # exclude objects.7z
-        metadata_zip,  # destination
-        aip_dir  # source
-    ]
-    subprocess.call(command)
+    # metadata_zip = os.path.join(aip_dir + "-temp", "metadata.7z")
+    # command = [
+        # "7z", "a",  # add
+        # "-bd",  # disable percentage indicator
+        # "-t7z",  # type of archive
+        # "-y",  # assume yes on all queries
+        # "-m0=bzip2",  # compression method
+        # "-mtc=on", "-mtm=on", "-mta=on",  # keep timestamps (create, mod, access)
+        # "-mmt=on",  # multithreaded
+        # "-x!" + objects_zip,  # exclude objects.7z
+        # metadata_zip,  # destination
+        # aip_dir  # source
+    # ]
+    # subprocess.call(command)
+
+    # shutil.rmtree(aip_dir)
+
+    # os.rename(aip_dir + "-temp", aip_dir)
+
+    print "  * depositing to deep blue"
+
+    dspace_url = "https://dev.deepblue.lib.umich.edu"
+
+    url = dspace_url + "/RESTapi/login"
+    body = {"email": dspace_username, "password": dspace_password}
+    response = requests.post(url, json=body)
+
+    dspace_token = response.text
+
+    request = E.request(
+        E.collectionId("1047"),
+        E.metadata(
+            E.field(
+                E.name("dc.title"),
+                E.value(dcterms_title)
+            ),
+            E.field(
+                E.name("dc.description.abstract"),
+                E.value(dcterms_description_abstract)
+            ),
+            E.field(
+                E.name("dc.contributor.author"),
+                E.value(dcterms_contributor_author)
+            ),
+            E.field(
+                E.name("dc.date.issued"),
+                E.value(dcterms_date_issued)
+            ),
+            E.field(
+                E.name("dc.rights.copyright"),
+                E.value(dcterms_rights_copyright)
+            ),
+            E.field(
+                E.name("dc.relation.ispartofseries"),
+                E.value(dcterms_relation_ispartofseries)
+            ),
+        ),
+        E.bundles(
+            E.bundle(
+                E.name("ORIGINAL"),
+                E.bitstreams(
+                    E.bitstream(
+                        E.name("objects.7z"),
+                        E.mimeType("application/zip"),
+                        E.description("Archival material.")
+                    ),
+                    E.bitstream(
+                        E.name("metadata.7z"),
+                        E.mimeType("application/zip"),
+                        E.description("Administrative information. Access restricted to Bentley staff")
+                    )
+                )
+            )
+        )
+    )
+
+    package = etree.tostring(request, pretty_print=True, xml_declaration=True, encoding="utf-8")
+    with open(os.path.join(aip_dir, "package.xml"), mode="w") as f:
+        f.write(package)
+
+    print "  * notifying processor"
+
+    slack = Slacker(slack_token)
+    slack.chat.post_message("#digital-processing",
+        "@" + username + ": check out this item in Deep Blue dev!",
+        username="Deep Blue Bot",
+        icon_url="http://www.infodocket.com/wp-content/uploads/2016/09/2016-09-20_10-00-21.jpeg",
+        attachments=[
+            {
+                "author_name": dcterms_contributor_author,
+                "title": dcterms_title,
+                "title_link": "",
+                "text": dcterms_description_abstract
+            }
+        ]
+    )
